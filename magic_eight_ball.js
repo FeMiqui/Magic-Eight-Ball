3f1c5c6e8518527c1a32bd9e9444df1f74b522cb
FeMiqui <51029840+FeMiqui@users.noreply.github.com>
2020-01-13 18:04:43 -0300

    Create magic_eight_ball.js

---
+++ magic_eight_ball.js
@@ -0,0 +1,50 @@
+let userName = 'Jane';
+
+userName === 'Jane'? console.log('Hello, Jane!'): console.log('Hello!'); 
+
+var userQuestion = 'Is Math Science?'
+
+console.log(`${userName} asked: ${userQuestion}`);
+
+//Calculation of a random number between 0 and 7
+var randomNumber = Math.floor(Math.random() * 8);
+
+//response given by the eightBall
+let eightBall = '';
+
+//Saved answers
+switch(randomNumber){
+  case 0:
+    eightBall ='It is certain';
+    break;
+       
+  case 1:
+    eightBall ='It is decidedly so';
+    break;
+       
+   case 2:
+     eightBall ='Reply hazy try again';   
+     break;
+       
+   case 3:
+     eightBall ='Cannot predict now';   
+     break;
+       
+    case 4:
+     eightBall ='Do not count on it';  
+      break;
+       
+    case 5:
+      eightBall ='My sources say no';  
+      break;
+       
+    case 6:
+      eightBall ='Outlook not so good';  
+      break;
+       
+    case 7:
+      eightBall ='Signs point to yes';  
+      break;    
+}
+
+console.log(eightBall);
